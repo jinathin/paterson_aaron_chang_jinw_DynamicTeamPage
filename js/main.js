6a386d31a6b88186c7d298847f3da5f4c9970f5f
Your Name <jinathin@gmail.com>
2019-03-16 23:04:26 -0400

    js working only 50%

--- js/main.js
+++ js/main.js
@@ -1,34 +1,77 @@
-(function(){
+
+
+//console.log("Hello " + firstName);
+// console.log("javascript linked up");
+
+// Show an element
+let show = function (elem) {
+
+  // Get the natural height of the element
+  let getHeight = function () {
+    elem.style.display = 'block'; // Make it visible
+    let height = elem.scrollHeight + 'px'; // Get it's height
+    elem.style.display = ''; //  Hide it again
+    return height;
+  };
+
+  let height = getHeight(); // Get the natural height
+  elem.classList.add('is-visible'); // Make the element visible
+  elem.style.height = height; // Update the max-height
+
+  // Once the transition is complete, remove the inline max-height so the content can scale responsively
+  window.setTimeout(function () {
+    elem.style.height = '';
+  }, 350);
+
+};
+
+// Hide an element
+let hide = function (elem) {
+
+  // Give the element a height to change from
+  elem.style.height = elem.scrollHeight + 'px';
+
+  // Set the height back to 0
+  window.setTimeout(function () {
+    elem.style.height = '0';
+  }, 1);
+
+  // When the transition is complete, hide it
+  window.setTimeout(function () {
+    elem.classList.remove('is-visible');
+  }, 350);
+
+};
+
+// Toggle element visibility
+let toggle = function (elem, timing) {
+
+  // If the element is visible, hide it
+  if (elem.classList.contains('is-visible')) {
+    hide(elem);
+    return;
+  }
+
+  // Otherwise, show it
+  show(elem);
   
-  var windowH = $(window).height(),
-      documElem = $(document),
-      slideDownPage = $('.slide-down-page'),
-      content = $('.content'),
-      btns = $('.btn'),
-      animSpeed = 700;
-  
-  slideDownPage.css({
-      height: windowH + 'px',
-      top: -windowH + 'px'
-  });
-  
-  btns.on('click', function(e) {
-      if( $(this).hasClass('open') ) {
-        slideDownPage.animate({ 'top': 0}, animSpeed);
-        content.animate({ 'margin-top': windowH + 'px'}, animsSpeed);
-      } else {
-        slideDownPage.animate({ 'top': -windowH + 'px' }, animSpeed);
-        content.animate({ 'margin-top': 0 }, animSpeed);
-      }
-    e.preventDefault();
-  });
-  
-  documElem.on('scroll', function() {
-    if ( $(this).scrollTop() > slideDownPage.height() && slideDownPage.css('top') === '0px' ) {
-      slideDownPage.css('top', -windowH + 'px');
-      content.css('margin-top', 0);
-      documElem.scrollTop(0);
-    }
-  });
-  
-})();
+};
+
+// Listen for click events
+document.addEventListener('click', function (event) {
+
+  // Make sure clicked element is our toggle
+  if (!event.target.classList.contains('toggle')) return;
+
+  // Prevent default link behavior
+  event.preventDefault();
+
+  // Get the content
+  let content = document.querySelector(".btn1");
+  if (!content) return;
+
+  // Toggle the content
+  toggle(content);
+  console.log(content)
+
+}, false);
